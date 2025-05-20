2db9db2e2dfbb9dcb5ce8f66416f820a209a51e4
Ryan Carsten Schmidt <git@ryandesign.com>
2025-05-20 00:41:35 -0500

    Also import compiled program if present on dsk

--- import.sh
+++ import.sh
@@ -26,8 +26,12 @@ trap cleanup EXIT
 printf "Mount %s\n" "$dsk"
 hmount "$dsk" || exit $?
 
+# The macbinary tool won't write to a file that exists, so we must make a
+# temporary directory for it to write into. Its manpage documents a --force flag
+# that can be used to make it write to an existing file, but the tool doesn't
+# actually have any such option. Bug report filed with Apple as FB17654870.
 tmpdir=$(mktemp -d "$TMPDIR/import.XXXXXXXX")
-binfile="$tmpdir/tmp.bin"
+tmpfile="$tmpdir/tmp"
 
 import_file() {
     local method="$1"
@@ -35,24 +39,30 @@ import_file() {
     local infile
     infile=$(printf %s "$outfile" | iconv -f utf-8 -t macroman)
     printf "Import %s\n" "$outfile"
-    hcopy -m ":$infile" - | macbinary decode --pipe -o "$binfile" || return $?
-    case $method in
-        rez)
-            DeRez "$binfile" "$SDK/RIncludes/Types.r" \
-                -d ALRT_RezTemplateVersion=0 \
-                -d DLOG_RezTemplateVersion=0 \
-                | LC_CTYPE=C sed -E 's/^(\t\$"[^"]*").*/\1/' \
-                | iconv -f macroman -t utf-8 \
-                | "$scriptdir/sortrez.pl" > "$outfile.r" || return $?
-            ;;
-        text)
-            iconv -f macroman -t utf-8 < "$binfile" \
-                | tr '\r' '\n' > "$outfile" || return $?
-            ;;
-    esac
-    rm -f "$binfile"
+    if [ "$method" = "macbinary" ]; then
+        hcopy -m ":$infile" "$outfile.bin" || return $?
+    else
+        hcopy -m ":$infile" - \
+            | macbinary decode --pipe -o "$tmpfile" || return $?
+        case $method in
+            rez)
+                DeRez "$tmpfile" "$SDK/RIncludes/Types.r" \
+                    -d ALRT_RezTemplateVersion=0 \
+                    -d DLOG_RezTemplateVersion=0 \
+                    | LC_CTYPE=C sed -E 's/^(\t\$"[^"]*").*/\1/' \
+                    | iconv -f macroman -t utf-8 \
+                    | "$scriptdir/sortrez.pl" > "$outfile.r" || return $?
+                ;;
+            text)
+                iconv -f macroman -t utf-8 < "$tmpfile" \
+                    | tr '\r' '\n' > "$outfile" || return $?
+                ;;
+        esac
+        rm -f "$tmpfile"
+    fi
 }
 
 import_file rez "$proj.π" || exit $?
 import_file rez "$proj.π.rsrc" || exit $?
 import_file text "$proj.c" || exit $?
+[ -n "$(hls "$proj" 2>/dev/null)" ] && import_file macbinary "$proj" || exit $?
